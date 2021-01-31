14556c6946bc8edf1e3150f4b1f8cc2aec71ae7f
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-31 18:31:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,8 @@ def payment1(message):
 	msg = bot.reply_to(message, 'Введите номер, сумму, комментарий в таком формате: 79998882233|200|coment')
 	bot.register_next_step_handler(msg, payment2)
 def payment2(message):
-	msg = message.text.split('')
-	paymentarray.append(msg[0]);paymentarray.append(msg[1]);paymentarray.append(msg[2]);
-	msg2 = bot.reply_to(message, 'Вы хотите отправить '+msg[1]+' руб. на номер '+msg[0]+' с комментарием: '+msg[2]+' ?',reply_markup=keyboard2)
+	paymentarray.append(msg[0]);
+	msg2 = bot.reply_to(message, 'Вы хотите отправить руб. на номер '+msg[0]+' с комментарием: ?',reply_markup=keyboard2)
 	bot.register_next_step_handler(msg2, payment3)
 def payment3(message):
 	if message.text.lower() == 'да':
