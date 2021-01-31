9b73d4cc696c0f47002adf1a2f4bd775c25b8a3b
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-31 16:00:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ def welcome(message):
     bot.register_next_step_handler(sent_msg, name_handler)
     
 def name_handler(message):
-    username666 = message.text
-    sent_msg = bot.send_message(message.chat.id, "Your name is ", username666)
-    bot.register_next_step_handler(sent_msg, username666) #Next message will call the age_handler function
+    name = message.text
+    sent_msg = bot.send_message(message.chat.id, "Your name is ", name)
+    bot.register_next_step_handler(sent_msg, name) #Next message will call the age_handler function
 
     
     
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def handle_text(message):
     if message.text == "🤖 BTC Banker":
-        request = requests.get('https://github.com/', username666)
+        request = requests.get('https://github.com/', name)
         if request.status_code == 200:
             bot.send_message(message.chat.id, "+", parse_mode='Markdown')
         else:
