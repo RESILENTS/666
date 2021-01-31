62442cec52fd92fabbc60ee8ea4ac24f5c453d55
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-31 16:13:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ def handle_text(message):
     if message.text == "🤖 BTC Banker":
         request = requests.get('https://github.com/')
         if request.status_code == 200:
-            bot.send_message(message.chat.id, "+" + txt, parse_mode='Markdown')
+            bot.send_message(message.chat.id, "+", parse_mode='Markdown')
         else:
-            bot.send_message(message.chat.id, "-" + txt, parse_mode='Markdown')
+            bot.send_message(message.chat.id, "-", parse_mode='Markdown')
             
     if message.text == "🤖 Chatex Bot":
         request = requests.get('https://github.com/b567567567567')
