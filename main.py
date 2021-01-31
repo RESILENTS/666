d71a1eb9ecc16b09115b659d3c74a389fbc60d2d
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-31 21:26:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,41 +7,197 @@ import requests
 token = '1434012352:AAG4yCSwZBi8PafX8hzR9ac7Xd_bNqnIZsE'
 bot = telebot.TeleBot(token)
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('/pay')
-keyboard1.row('/balance')
-keyboard1.row('/history')
-keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard2.row('666')
-keyboard2.row('777')
 
-users = [936565964]
-paymentarray = []
+#searches for usernames
+def usernameSearch():
+    username = input("Enter Username: ").lower()
+    print()
+    #instagram user search
+    print('''indicates username taken [-]
+indicates username available [+]''')
+    instaurl = 'https://instagram.com/' + username
+    instaresponse = get(instaurl, headers=headers)
+    if instaresponse.status_code == 200:
+        print('instagram [-]')
+    else:
+        print('instagram [+]')
     
-@bot.message_handler(commands=['start'])
-def payment1(message):
-	msg = bot.reply_to(message, 'Введите номер, сумму, комментарий в таком формате: 79998882233|200|coment')
-	bot.register_next_step_handler(msg, payment2)
-def payment2(message):
-	msg = message.text.split()
-	paymentarray.append(msg[0]);
-	msg2 = bot.reply_to(message, 'Вы хотите отправить руб. на номер '+msg[0]+' с комментарием: ?',reply_markup=keyboard2)
-	bot.register_next_step_handler(msg2, payment3)
+    #twitter user search
+    twitterurl = 'https://twitter.com/' + username
+    twitterresponse = get(twitterurl, headers=headers)
+    if twitterresponse.status_code == 200:
+        print('Twitter [-]')
+    else:
+        print('Twitter [+]')
 
+    #tumblr user search
+    tumblrurl = 'https://' + username + '.' + 'tumblr.com'
+    tumblrresponse = get(tumblrurl, headers=headers)
+    if tumblrresponse.status_code == 200:
+        print('Tumblr [-]')
+    else:
+        print('Tumblr [+]')
 
-@bot.message_handler(commands=['pay'])
-def payment1(message):
-	msg = bot.reply_to(message, 'Введите номер, сумму, комментарий в таком формате: 79998882233|200|coment')
-	bot.register_next_step_handler(msg, payment2)
+    #viber user search
+    viberurl = 'https://chats.viber.com/' + username
+    viberresponse = get(viberurl, headers=headers)
+    if viberresponse.status_code == 200:
+        print('Viber [-]')
+    else:
+        print('Viber [+]')
+
+    #vk user search
+    vkurl = 'https://vk.com/' + username
+    vkresponse = get(vkurl, headers=headers)
+    if vkresponse.status_code == 200:
+        print('VK [-]')
+    else:
+        print('VK [+]')
+
+    #pinterest user search
+    pinteresturl = 'https://pinterest.com/' + username
+    pinterestresponse = get(pinteresturl, headers=headers)
+    if pinterestresponse.status_code == 200:
+        print('Pinterest [-]')
+    else:
+        print('Pinterest [+]')
+
+    #reddit user search
+    redditurl = 'https://reddit.com/user/' + username
+    redditresponse = get(redditurl, headers=headers)
+    if redditresponse.status_code == 200:
+        print('Reddit [-]')
+    else:
+        print('Reddit [+]')
+
+    #taringa user search
+    taringaurl = 'https://taringa.net/' + username
+    taringaresponse = get(taringaurl, headers=headers)
+    if taringaresponse.status_code == 200:
+        print('Taringa [-]')
+    else:
+        print('Taringa [+]')
+
+    #tagged user search
+    taggedurl = 'http://tagged.com/' + username
+    taggedresponse = get(taggedurl, headers=headers)
+    if taggedresponse.status_code == 200:
+        print('Tagged [-]')
+    else:
+        print('Tagged [+]')
+
+    #badoo user search
+    badoourl = 'https://badoo.com/profile/' + username
+    badooresponse = get(badoourl, headers=headers)
+    if badooresponse.status_code == 200:
+        print('Badoo [-]')
+    else:
+        print('Badoo [+]')
+
+    #github user search
+    githuburl = 'https://github.com/' + username
+    githubresponse = get(githuburl, headers=headers)
+    if githubresponse.status_code == 200:
+        print('Github [-]')
+    else:
+        print('Github [+]')
     
-    
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def payment2(message):
-	msg = message.text.split()
-	paymentarray.append(msg[0]);
-	msg2 = msg[0]
+    #myspace user search
+    myspaceurl = 'https://myspace.com/' + username
+    myspaceresponse = get(myspaceurl, headers=headers)
+    if myspaceresponse.status_code == 200:
+        print('Myspace [-]')
+    else:
+        print('Myspace [+]')
 
-def handle_text(message):
+    #mix user search (originally stumbledupon
+    mixurl = 'https://mix.com/' + username
+    mixresponse = get(mixurl, headers=headers)
+    if mixresponse.status_code == 200:
+        print('Mix [-]')
+    else:
+        print('Mix [+]')
+
+    #wattpad user search
+    wattpadurl = 'https://wattpad.com/user/' + username
+    wattpadresponse = get(wattpadurl, headers=headers)
+    if wattpadresponse.status_code == 200:
+        print("Wattpad [-]")
+    else:
+        print("Wattpad [+]")
+
+    #deviantart user search
+    deviantarturl = 'https://deviantart.com/' + username
+    deviantartresponse = get(deviantarturl, headers=headers)
+    if deviantartresponse.status_code == 200:
+        print('Deviant Art [-]')
+    else:
+        print('Deviant Art [+]')
+
+    #goodreads user search
+    goodreadsurl = 'https://goodreads.com/' + username
+    goodreadsresponse = get(goodreadsurl, headers=headers)
+    if goodreadsresponse.status_code == 200:
+        print('Good Reads [-]')
+    else:
+        print('Good Reads [+]')
+    
+    #couchsurfing user search
+    couchsurfurl = 'https://couchsurfing.com/people/' + username
+    couchsurfresponse = get(couchsurfurl, headers=headers)
+    if couchsurfresponse.status_code == 200:
+        print('Couch Surfing [-]')
+    else:
+        print('Couch Surfing [+]')
+    
+    #ask.fm user search
+    askfmurl = 'https://ask.fm/' + username
+    askfmresponse = get(askfmurl, headers=headers)
+    if askfmresponse.status_code == 200:
+        print('Ask Fm [-]')
+    else:
+        print('Ask Fm [+]')
+
+    #flickr user search
+    flickrurl = 'https://flickr.com/' + username
+    flickrresponse = get(flickrurl, headers=headers)
+    if flickrresponse.status_code == 200:
+        print('Flickr [-]')
+    else:
+        print('Flickr [+]')
+
+    #imgur user search
+    imgururl = 'https://imgur.com/user/' + username
+    imgurresponse = get(imgururl, headers=headers)
+    if imgurresponse.status_code == 200:
+        print('Imgur [-]')
+    else:
+        print('Imgur [+]')
+
+    #soundcloud user search
+    scurl = 'https://soundcloud.com/' + username
+    scresponse = get(scurl, headers=headers)
+    if scresponse.status_code == 200:
+        print('Soundcloud [-]')
+    else:
+        print('Soundcloud [+]')
+
+    #minds user search
+    mindsurl = 'https://minds.com/' + username
+    mindsresponse = get(mindsurl, headers=headers)
+    if mindsresponse.status_code == 200:
+        print('Minds [-]')
+    else:
+        print('Minds [+]')
+
+    #delicious user search
+    deliciousurl = 'https://del.icio.us/' + username
+    deliciousresponse = get(deliciousurl, headers=headers)
+    if deliciousresponse.status_code == 200:
+        print('Delicious [-]')
+    else:
+        print('Delicious [+]')
+
     if message.text == "666":
         request = requests.get('https://github.com/')
         if request.status_code == 200:
