05b92c5cda5aca2c639183333856fcebb5a8c3a0
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 03:39:17 +0200

    Update username_check.py

--- username_check.py
+++ username_check.py
@@ -4,7 +4,7 @@ import time
 def usernameSearch(): 
     twitterurl = 'https://twitter.com/' + username_check_a
     twitterresponse = get(twitterurl, headers=headers)
-        if twitterresponse.status_code == 200:
-            twitter_i = bot.send_message(message.from_user.id, ' ❌ *Twitter:* Не найдено.', parse_mode='Markdown')
-        else:
-            twitter_i = bot.send_message(message.from_user.id, ' ➖ *Twitter:* https://twitter.com/' + username_check_a, parse_mode='Markdown')
+    if twitterresponse.status_code == 200:
+        twitter_i = bot.send_message(message.from_user.id, ' ❌ *Twitter:* Не найдено.', parse_mode='Markdown')
+    else:
+        twitter_i = bot.send_message(message.from_user.id, ' ➖ *Twitter:* https://twitter.com/' + username_check_a, parse_mode='Markdown')
