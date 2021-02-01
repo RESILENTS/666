287aecaa9121cea75faf8b6ba0f8f2e0c5a68d1f
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 05:37:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import telebot, requests
+import telebot
 from telebot import types
+from requests import get
 import time, os
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
