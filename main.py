5d5d620c14f705a122df81e3abade0855c354bf0
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 02:37:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,28 +16,28 @@ car_make = ''
 car_model = ''
 
 @bot.message_handler(commands=['start'])
-def start(message: Message):
+def start(message):
     bot.send_message(message.from_user.id, 'Для начала нажмите необходимую кнопку', reply_markup=markup_menu)
 
 @bot.message_handler(content_types=['text'])
-def new_verify(message: Message):
+def new_verify(message):
     if message.text.lower() == 'запись нового авто':
         begin_new_car = bot.send_message(message.from_user.id, 'Введите регистрационный номер:')
         bot.register_next_step_handler(begin_new_car, get_car_plate)
 
-def get_car_plate(message: Message):
+def get_car_plate(message):
     global car_plate
     car_plate = message.text.upper()
     bot.send_message(message.from_user.id, 'Введите марку авто:')
     bot.register_next_step_handler(message, get_car_make)
 
-def get_car_make(message: Message):
+def get_car_make(message):
     global car_make
     car_make = message.text.upper()
     bot.send_message(message.from_user.id, 'Введите модель авто:')
     bot.register_next_step_handler(message, get_car_model)
 
-def get_car_model(message: Message):
+def get_car_model(message):
     global car_model
     car_model = message.text.upper()
     bot.send_message(message.from_user.id, '1:' + car_plate + '2: ' + car_make + '3: ' + car_model)
