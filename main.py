a13ffbf760d8b86e2befb3936399cec7dda61c53
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 03:15:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,7 @@ import username_check
 token = '1434012352:AAG4yCSwZBi8PafX8hzR9ac7Xd_bNqnIZsE'
 bot = telebot.TeleBot(token)
 
-markup_menu = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-btn_new = telebot.types.KeyboardButton('Запись нового авто')
-btn_verify = telebot.types.KeyboardButton('Проверка авто')
-markup_menu.add(btn_new, btn_verify)
-
-car_plate = ''
+username_check_a = ''
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -24,24 +19,12 @@ def start(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def handle_text(message):
     if message.text == '🔎 OSINT':
-        begin_new_car = bot.send_message(message.from_user.id, 'Введите регистрационный номер:')
-        bot.register_next_step_handler(begin_new_car, get_car_plate)
-
-def get_car_plate(message):
-    global car_plate
-    car_plate = message.text.upper()
-    bot.send_message(message.from_user.id, 'Введите марку авто:')
-    bot.register_next_step_handler(message, get_car_make)
-
-def get_car_make(message):
-    global car_make
-    car_make = message.text.upper()
-    bot.send_message(message.from_user.id, 'Введите модель авто:')
-    bot.register_next_step_handler(message, get_car_model)
+        username_check = bot.send_message(message.from_user.id, 'Введите регистрационный номер:')
+        bot.register_next_step_handler(username_check, get_car_model)
 
 def get_car_model(message):
     global car_model
     car_model = message.text.upper()
-    bot.send_message(message.from_user.id, '1:' + car_plate + '2: ' + car_make + '3: ' + car_model)
+    bot.send_message(message.from_user.id, '@username: ' + username_check_a')
 
 bot.polling(none_stop=True)
