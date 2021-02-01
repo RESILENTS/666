18e69a720bbd662fd8f01fad83a07db96efcde21
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 05:38:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from telebot import types
 from requests import get
 import time, os
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
 token = '1434012352:AAG4yCSwZBi8PafX8hzR9ac7Xd_bNqnIZsE'
 bot = telebot.TeleBot(token)
 
@@ -25,6 +23,8 @@ def handle_text(message):
         bot.register_next_step_handler(username_check, usernameSearch)
 
 def usernameSearch(message):
+    headers = {'Mozilla/5.0 (iPhone; CPU iPhone OS 7_2_1 like Mac OS X; en-US) AppleWebKit/534.9.4 (KHTML, like Gecko) Version/4.0.5 Mobile/8B115 Safari/6534.9.4'}
+    
     global username_check_a
     username_check_a = message.text.lower()
     
