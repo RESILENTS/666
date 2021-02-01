79ee7a3ac2e8bf1c3fb2eaed4299008a2ed6d99c
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 05:34:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import telebot, requests
 from telebot import types
 import time
-from fake_useragent import UserAgent
 
-ua = UserAgent()
-headers = {'User-Agent': ua.random}
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 response = requests.get(url, headers=headers)
 
 token = '1434012352:AAG4yCSwZBi8PafX8hzR9ac7Xd_bNqnIZsE'
