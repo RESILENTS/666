001df260d41863e7b03c9ed32a75b225dfd671ed
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 05:17:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,15 +26,14 @@ def handle_text(message):
 def usrchk(usernamee):
     global username_check_a
     username_check_a = message.text.lower()
-                            try:
-                                url = f"http://www.jeuxvideo.com/profil/{usernamee}"
-                                r = requests.get(url,headers=headers)
-                                re = str(r.status_code)
-                                if "404" in re:
-                                    pass
-                                elif "200" in re:
-                                    bot.send_message(message.from_user.id, ' ➖ *Twitter:* https://imgur.com/user/' + username_check_a, parse_mode='Markdown')
-                            try:
+                                
+        url = "http://www.jeuxvideo.com/profil/{usernamee}"
+        r = requests.get(url,headers=headers)
+        re = str(r.status_code)
+        if "404" in re:
+            pass
+        elif "200" in re:
+            bot.send_message(message.from_user.id, ' ➖ *Twitter:* https://imgur.com/user/' + username_check_a, parse_mode='Markdown')
                                 
 def user_actions():
     u = input("Username : ")
