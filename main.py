0ebb4cb9b2494c07b5065e767b4ffe4ede8801c2
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 02:24:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,34 @@ import telebot
 from telebot import types
 import random
 import time
-import string
 
 token = '1434012352:AAG4yCSwZBi8PafX8hzR9ac7Xd_bNqnIZsE'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
-def selfmyself(message):
+def first_q(message):
     service = telebot.types.ReplyKeyboardMarkup(True)
-    service.row('🤖 Chatex Bot', '🤖 BTC Banker')
+    service.row('Check 1', 'Check 2')
+    send = bot.send_message(message.chat.id, 'первый вопрос')
+    bot.register_next_step_handler(send, two_q, reply_markup=service)
     
-    message = bot.send_message(message.chat.id, "Введите ссылку на товар", disable_notification=True, reply_markup=service)
-    url = message.text
-    constants.items.append(url)
-    bot.register_next_step_handler(message, another_process)
+def two_q(message):
+    global answers
+    answers = []
+    first_answer = message.text
+    answers.append(first_answer)
+
+    send = bot.send_message(message.chat.id, 'второй вопрос')
+    bot.register_next_step_handler(send)
+    
+def end(message):
+    four_answer = message.text
+    answers.append(four_answer)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    if message.text == "🤖 BTC Banker":
-        new_pas = "https://t.me/BTC_CHANGE_BOT?start=с_"
-        bot.send_message(message.chat.id, new_pas, disable_web_page_preview=True)
+    if message.text == "Check 1":
+        bot.send_message(message.chat.id, '{}'.format(''.join(answers)))
         
     if message.text == "🤖 Chatex Bot":
         new_pas = "https://t.me/Chatex_bot?start=c_"
