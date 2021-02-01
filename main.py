8eac26ba7032ec3ea5a61d2b8ec2b933ec849e07
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 04:43:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telebot 
 from telebot import types
 import time
-from classes import username_check_a7
 
 token = '1434012352:AAG4yCSwZBi8PafX8hzR9ac7Xd_bNqnIZsE'
 bot = telebot.TeleBot(token)
@@ -15,4 +14,28 @@ def start(message):
     service.row('🔀 Разные', 'ℹ️ FAQ')
     bot.send_message(message.from_user.id, 'Для начала нажмите необходимую кнопку', reply_markup=service)
 
+@bot.message_handler(commands=['username_check'])
+def handle_text(message):
+        username_check = bot.send_message(message.from_user.id, '❗️ Введите ник пользователя без @:')
+        service2 = telebot.types.ReplyKeyboardMarkup(True)
+        service2.row('🔎 Начать поиск')
+        bot.register_next_step_handler(username_check, get_car_model, reply_markup2=service)
+
+def get_car_model(message):
+    global username_check_a
+    username_check_a = message.text.upper()
+    expire = usernameSearch(message.from_user.id)
+    bot.send_message(message.from_user.id, twitter_i)
+
+def usernameSearch(self): 
+    global username_check_a
+    
+    twitterurl = 'https://twitter.com/' + username_check_a
+    twitterresponse = get(twitterurl, headers=headers)
+    if twitterresponse.status_code == 200:
+        twitter_i = bot.send_message(message.from_user.id, ' ❌ *Twitter:* Не найдено.', parse_mode='Markdown')
+    else:
+        twitter_i = bot.send_message(message.from_user.id, ' ➖ *Twitter:* https://twitter.com/' + username_check_a, parse_mode='Markdown')
+        
+        
 bot.polling(none_stop=True)
