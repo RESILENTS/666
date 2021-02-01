27c882b85420197bac1919dc6fe017cb17199a6d
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 03:17:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def handle_text(message):
         bot.register_next_step_handler(username_check, get_car_model)
 
 def get_car_model(message):
-    global car_model
+    global username_check_a
     car_model = message.text.upper()
     bot.send_message(message.from_user.id, '@username: ' + username_check_a)
 
