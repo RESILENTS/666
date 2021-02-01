925754ed38a27278b286eb7f675216ead9c24681
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 04:23:16 +0200

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,7 @@
+class username_check_a7:
+    # конструктор
+    def __init__(self, name):
+        self.name = username_check_a
+ 
+    def display_info(self):
+        bot.send_message(message.chat.id, "Введите данные", self.name)
