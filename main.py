8c6353b65090ef3c1f0cba3be53e1bceee9d188e
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-02-01 05:42:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time, os
 token = '1434012352:AAG4yCSwZBi8PafX8hzR9ac7Xd_bNqnIZsE'
 bot = telebot.TeleBot(token)
 
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 username_check_a = ''
 
 @bot.message_handler(commands=['start'])
@@ -23,17 +24,16 @@ def handle_text(message):
         bot.register_next_step_handler(username_check, usernameSearch)
 
 def usernameSearch(message):
-    headers = {'Mozilla/5.0 (iPhone; CPU iPhone OS 7_2_1 like Mac OS X; en-US) AppleWebKit/534.9.4 (KHTML, like Gecko) Version/4.0.5 Mobile/8B115 Safari/6534.9.4'}
-    
     global username_check_a
     username_check_a = message.text.lower()
     
-    twitterurl = 'https://imgur.com/user/' + username_check_a
-    twitterresponse = get(twitterurl, headers=headers)
-    if twitterresponse.status_code == 200:
+    twitterurl = "https://imgur.com/user/" + username_check_a
+    page = requests.get(twitterurl) 
+    if page.status_code==200:
         bot.send_message(message.from_user.id, ' ❌ *Twitter:* https://imgur.com/user/' + username_check_a, parse_mode='Markdown')
     else:
         bot.send_message(message.from_user.id, ' ➖ *Twitter:* https://imgur.com/user/' + username_check_a, parse_mode='Markdown')
+    
                                 
         
 bot.polling(none_stop=True)
